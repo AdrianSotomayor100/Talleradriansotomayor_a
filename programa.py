b4bc0b5edcc5cbcf601bd3ebf557628d46ffc933
AdrianSotomayor100 <135990571+AdrianSotomayor100@users.noreply.github.com>
2023-06-15 14:54:24 +0000

    sdfgasdfasd

---
+++ programa.py
@@ -0,0 +1,8 @@
+
+from modulo_funciones import sumar, restar, multiplicar, dividir
+from algebra import distancia
+print(sumar(2, 3))
+print(restar(4, 2))
+print(multiplicar(7, 3))
+print(dividir(81, 9))
+print(distancia(1, 2, 3, 4))
